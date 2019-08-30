54973cbe9110376f4393faa4786a42ceb568a6d6
Your Name <nguyenphuthang73@gmail.com>
2019-08-30 22:18:49 +0700

    register

--- mp3-angular/src/app/app.module.ts
+++ mp3-angular/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { UserActionComponent } from './components/user-action/user-action.component';
 import { UserInformationComponent } from './components/user-information/user-information.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserActionComponent,
-    UserInformationComponent
+    UserInformationComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mp3-angular/src/app/components/register/register.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { FormGroup, Validators, AbstractControl, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterService } from 'src/app/service/register.service';
+import { IUser } from 'src/app/model/user/user.model';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  errorMessage:any;
+  isRegisterFailed = false;
+  constructor(private registerService: RegisterService, private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      id: [''],
+      name: [''],
+      age: ['',],
+      gender: [''],
+      phone: ['', [Validators.required, Validators.pattern(/^\+84\d{9,10}$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      delected: [false]
+    });
+  }
+  get username(): AbstractControl {
+    return this.registerForm.get('username');
+  };
+
+  get password(): AbstractControl {
+    return this.registerForm.get('password');
+  };
+  get email(): AbstractControl {
+    return this.registerForm.get('email');
+  };
+  get phone(): AbstractControl {
+    return this.registerForm.get('phone');
+  }
+  createForm() {
+    if (this.registerForm.valid) {
+      const { value } = this.registerForm;
+      this.registerService.register(value).subscribe(
+        next => {
+          alert("Successful");
+
+        },
+        error => {
+          this.errorMessage = error.error;
+          console.log(this.errorMessage);
+          this.isRegisterFailed = true;
+        }
+      );
+    }
+  }
+}
+
+
